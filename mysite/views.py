3ae81d58147196b93eb06be1316a9dea4365a704
JeonChangJun <jcjk0302@gmail.com>
2022-02-04 14:21:30 +0900

    extend base.html

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 # -- templateView
 class HomeView(TemplateView):
-  template_name = 'home.html'
+  template_name = 'home.html' # TemplateView를 상속 받아 사용하는경우 필수적으로 template_name 클래스 변수를 오버라이딩으로 지정해야함
